e2f481afc227d9eee3693d07cb6daee853076605
javabull-dev <fengechui@foxmail.com>
2025-03-02 15:21:27 +0800

    feature 添加HelloController

---
+++ src/test/java/cn/ljpc/jenkinsproject/JenkinsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.ljpc.jenkinsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
